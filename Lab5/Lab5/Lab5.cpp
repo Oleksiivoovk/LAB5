12a3adbbf67f8267e03dd1c13c1a53643fa92ec6
Oleksiivoovk <114281831+Oleksiivoovk@users.noreply.github.com>
2022-12-20 16:48:33 +0200

    Update Lab5.cpp

--- Lab5/Lab5/Lab5.cpp
+++ Lab5/Lab5/Lab5.cpp
@@ -1,20 +1,52 @@
-﻿// Lab5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+	
+	int a[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,};
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+	cout << "elements" << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << a[i] << endl;
+	}
+	int* pa1, * pa2;
+	
+	
+	int x, y;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	cout << "number of elements 1: ";
+	cin >> x;
+
+	cout << "number of elements 2: ";
+	cin >> y;
+
+	pa1 = &a[x];
+	pa2 = &a[y];
+	
+	cout << "-------------------------------------------" << endl;
+	cout << *pa1 << endl;
+	cout << "adress 1:";
+	cout << pa1  << endl;
+	
+	cout << "-------------------------------------------" << endl;
+
+	cout << *pa2 << endl;
+	cout << "adress 2:";
+	cout << pa2 << endl;
+	
+	cout << "-------------------------------------------" << endl;
+	
+	int q;
+	q = *pa1;
+	*pa1 = *pa2;
+	*pa2 = q;
+	
+	
+	for (int i = 0; i < 10; i++)
+	{
+		cout << a[i] << endl;
+	}
+    
